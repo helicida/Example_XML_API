92b6cb9f884e31f405b279d3d1548b33dc9aedbf
Sergi Barjola <sergibca@gmailcom>
2016-03-31 20:32:34 +0200

    Se ha empezado a escribir el javadoc

--- src/DAO/eXistDB_DAO/DAO.java
+++ src/DAO/eXistDB_DAO/DAO.java
@@ -9,24 +9,34 @@ import org.xmldb.api.base.XMLDBException;
 import org.xmldb.api.modules.CollectionManagementService;
 import javax.xml.xquery.*;
 import java.io.File;
+import java.io.Serializable;
 
 /**
  * Created by 46465442z on 31/03/16.
  */
-public class DAO {
+public class DAO implements Serializable{
 
     private String URI = "";
     private String ip = "";
     private String puerto = "";
     private String usuario = "";
     private String contrasenya = "";
-
     private final String driver = "org.exist.xmldb.DatabaseImpl";
 
+    /**
+     *
+     */
     public DAO(){
 
     }
 
+    /**
+     *
+     * @param ip
+     * @param puerto
+     * @param usuario
+     * @param contrasenya
+     */
     public DAO(String ip, String puerto, String usuario, String contrasenya){
         this.ip = ip;
         this.puerto = puerto;
@@ -35,6 +45,14 @@ public class DAO {
         this.URI = "xmldb:exist://" + ip + ":" + puerto + "/exist/xmlrpc/db";
     }
 
+    /**
+     *
+     * @param nombreColeccion
+     * @throws XMLDBException
+     * @throws ClassNotFoundException
+     * @throws IllegalAccessException
+     * @throws InstantiationException
+     */
     public void crearColeccio(String nombreColeccion) throws XMLDBException, ClassNotFoundException, IllegalAccessException, InstantiationException {
         //**************
         // Inicializamos los drivers de la BBDD
@@ -56,6 +74,12 @@ public class DAO {
         c.createCollection(nombreColeccion);
     }
 
+    /**
+     *
+     * @param rutaArchivo
+     * @param nombreColeccion
+     * @throws XMLDBException
+     */
     public void afegirRecurs (String rutaArchivo, String nombreColeccion) throws XMLDBException {
 
         // File del archivo que queremos subir como recurso
@@ -71,6 +95,12 @@ public class DAO {
         col.storeResource(res);
     }
 
+    /**
+     *
+     * @param xQuery
+     * @return
+     * @throws XQException
+     */
     public String consulta (String xQuery) throws XQException {
 
         String resultado = "";
@@ -97,5 +127,98 @@ public class DAO {
 
         // Devolvemos la respuesta
         return resultado;
+    }
+
+    // Getters
+
+    /**
+     *
+     * @return
+     */
+    public String getURI() {
+        return URI;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getIp() {
+        return ip;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getPuerto() {
+        return puerto;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getUsuario() {
+        return usuario;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getContrasenya() {
+        return contrasenya;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getDriver() {
+        return driver;
+    }
+
+
+    // Setters
+
+    /**
+     *
+     * @param URI
+     */
+    public void setURI(String URI) {
+        this.URI = URI;
+    }
+
+    /**
+     *
+     * @param ip
+     */
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    /**
+     *
+     * @param puerto
+     */
+    public void setPuerto(String puerto) {
+        this.puerto = puerto;
+    }
+
+    /**
+     *
+     * @param usuario
+     */
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    /**
+     *
+     * @param contrasenya
+     */
+    public void setContrasenya(String contrasenya) {
+        this.contrasenya = contrasenya;
     }
 }
