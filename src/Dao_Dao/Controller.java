7222a2611932137d7371b9e676018aa4f6de7414
Sergi Barjola <sergibca@gmail.com>
2016-04-19 14:05:13 +0200

    Algunos metodos para hacer consultas creados

--- src/Dao_Dao/Controller.java
+++ src/Dao_Dao/Controller.java
@@ -15,16 +15,9 @@ public class Controller {
 
     public static void main(String[] args) throws JAXBException {
 
-        // JAXB Controler
-        JAXBContext JaxbContext = JAXBContext.newInstance(TiendaType.class);
-        Unmarshaller ums = JaxbContext.createUnmarshaller();
-        TiendaType bbddTienda = (TiendaType) ums.unmarshal(new File("/home/46465442z/IdeaProjects/Example_XML_API/src/Dao_Dao/bbdd.xml"));
-
-        System.out.println(bbddTienda.getClientes().getCliente().get(0).getDni());
-
         // DAO's
          DAO_Diego DAO_eXists  = new DAO_Diego("172.31.101.225", "8080", "admin", "dionis");
-         DAO_XML DAO_XML = new DAO_XML("172.31.101.225", "8080", "coleccionAModificar", "/Dao_Dao/bbdd.xml");
+         DAO_XML DAO_XML = new DAO_XML("172.31.101.225", "8080", "coleccionAModificar", "/home/sergi/IdeaProjects/Example_XML_API/src/Dao_Dao/bbdd.xml");
 
         // Menú
         Scanner teclat = new Scanner(System.in);
@@ -37,18 +30,17 @@ public class Controller {
             System.out.println("MENU:");
 
             System.out.println("--------------Menu--------------");
-            System.out.println("1 - Mostrar todo");
-            System.out.println("2 - Añadir Empleado");
-            System.out.println("3 - Añadir Cliente");
-            System.out.println("4 - Añadir Factura");
-            System.out.println("5 - Añadir Producto");
-            System.out.println("6 - Borrar Empleado ");
-            System.out.println("7 - Borrar Cliente");
-            System.out.println("8 - Facturas Clientes");
-            System.out.println("9 - Empleado por nombre");
-            System.out.println("10 - Empleado por apellido");
-            System.out.println("11 - Empleado por salario");
-            System.out.println("12 -  Empleado por años trabajados");
+            System.out.println("1 - Añadir Empleado");
+            System.out.println("2 - Añadir Cliente");
+            System.out.println("3 - Añadir Factura");
+            System.out.println("4 - Añadir Producto");
+            System.out.println("5 - Borrar Empleado ");
+            System.out.println("6 - Borrar Cliente");
+            System.out.println("7 - Facturas de un cliente");
+            System.out.println("8 - Empleado por nombre");
+            System.out.println("9 - Empleado por apellido");
+            System.out.println("10 - Empleado por sueldo");
+            System.out.println("11 -  Empleado por años trabajados");
 
             System.out.println("0 - Sortir ");
 
@@ -61,28 +53,28 @@ public class Controller {
                 break;
 
                 case 1:
-                    //
-
-                break;
-
-                case 2:
                     // Anyadir un empleado al XML
                     DAO_XML.anyadirEmpleado();
                 break;
 
-                case 3:
+                case 2:
                     // Anyadir un cliente al XML
                     DAO_XML.anyadirCliente();
                 break;
 
-                case 4:
+                case 3:
                    // Anyadir una factura al XML
                     DAO_XML.anyadirFactura();
                 break;
 
-                case 5:
+                case 4:
                     // Anyadir un producto al XML
                     DAO_XML.anyadirProducto();
+                break;
+
+                case 5:
+                    //
+
                 break;
 
                 case 6:
@@ -91,33 +83,28 @@ public class Controller {
                 break;
 
                 case 7:
-                    //
-
+                    // Buscar las facturas de un cliente
+                    DAO_XML.facturasClientes();
                 break;
 
                 case 8:
-                    //
-
+                    // Buscar empleados por nombre
+                    DAO_XML.empleadosNombre();
                 break;
 
                 case 9:
-                    //
-
+                    // Buscar empleados por apellidos
+                    DAO_XML.empleadosApellidos();
                 break;
 
                 case 10:
-                    //
-
+                    // Buscar empleados por su sueldo
+                    DAO_XML.empleadosSalario();
                 break;
 
                 case 11:
-                    //
-
-                break;
-
-                case 12:
-                    //
-
+                    // Buscar empleados por antiguedad
+                    DAO_XML.empleadosAntiguedad();
                 break;
             }
         }
--- src/Dao_Dao/DAO_XML.java
+++ src/Dao_Dao/DAO_XML.java
@@ -1,9 +1,11 @@
 package Dao_Dao;
 
+import Dao_Dao.generated.*;
 import org.xmldb.api.base.XMLDBException;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
 import javax.xml.xquery.XQException;
 import java.io.File;
 import java.util.Scanner;
@@ -21,6 +23,8 @@ public class DAO_XML {
     private String coleccion = "";
     private String ruta_bbdd_XML = "";
     private File archivoBBDD;
+    private TiendaType raizBBDD;
+    private JAXBContext JaxbContext;
 
     // Constante que contiene el driver XML
     private final String DRIVER = "org.exist.xmldb.DatabaseImpl";
@@ -50,6 +54,16 @@ public class DAO_XML {
             System.out.println("No se ha encontrado la BBDD indicada");
         }
 
+        try {
+            JaxbContext = JAXBContext.newInstance(TiendaType.class);
+            Unmarshaller ums = JaxbContext.createUnmarshaller();
+            raizBBDD  = (TiendaType) ums.unmarshal(archivoBBDD);
+
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+;
+
     }
 
     /**
@@ -77,11 +91,11 @@ public class DAO_XML {
         System.out.println("Introduce el apellido del cliente");
             String apellido = teclat.nextLine();
 
-        /*
-        Cliente newClient = new Cliente();
-        newClient.setDNI(dni);
-        newClient.setNombre(nombre);
-        newClient.setApellido(apellido);*/
+
+        ClienteType cliente = new ClienteType();
+        cliente.setDni(dni);
+        cliente.setNombre(nombre);
+        cliente.setApellido(apellido);
 
     }
 
@@ -102,13 +116,13 @@ public class DAO_XML {
         System.out.println("Introduce la antiguedad del trabajador");
             String antiguedad = teclat.nextLine();
 
-        /*
-        Empleado newEmployee = new Empleado();
-        newEmployee.setID(id);
-        newEmployee.setNombre(nombre);
-        newEmployee.setApellidos(apellido);
-        newEmployee.setSueldo(sueldo);
-        newEmployee.setAntiguedad(antiguedad); */
+
+        EmpleadoType empleado = new EmpleadoType();
+        empleado.setId(id);
+        empleado.setNombre(nombre);
+        empleado.setApellido(apellido);
+        empleado.setSueldo(sueldo);
+        empleado.setAntiguedad(antiguedad);
 
     }
 
@@ -161,4 +175,113 @@ public class DAO_XML {
         producto.setStock(stock);*/
 
     }
+
+    // Consultas
+
+    void empleadosNombre() {
+
+        System.out.println("Introduce el nombre del empleado que quieres buscar");
+            String nombre = teclat.nextLine();
+
+        System.out.println("ID       | Nombre    | Apellidos     | Sueldo\n---------------------------------------");
+
+        for (int iterador = 0; iterador < raizBBDD.getEmpleados().getEmpleado().size(); iterador++)  {
+
+            EmpleadoType empleado = raizBBDD.getEmpleados().getEmpleado().get(iterador);
+
+            if (raizBBDD.getEmpleados().getEmpleado().get(iterador).getNombre().equalsIgnoreCase(nombre)) {
+                System.out.println(empleado.getId() + " - " +
+                        empleado.getNombre() + "  -  " +
+                        empleado.getApellido() + "  ->  " +
+                        empleado.getSueldo());
+            }
+        }
+    }
+
+    void empleadosApellidos(){
+
+        System.out.println("Introduce el apellido del empleado que quieres bucar");
+            String apellidos = teclat.nextLine();
+
+        System.out.println("ID       | Nombre    | Apellidos     | Sueldo\n---------------------------------------");
+
+        for (int iterador = 0; iterador < raizBBDD.getEmpleados().getEmpleado().size(); iterador++)  {
+
+            EmpleadoType empleado = raizBBDD.getEmpleados().getEmpleado().get(iterador);
+
+            if (raizBBDD.getEmpleados().getEmpleado().get(iterador).getApellido().equalsIgnoreCase(apellidos)) {
+                System.out.println(empleado.getId() + " - " +
+                        empleado.getNombre() + "  -  " +
+                        empleado.getApellido() + "  ->  " +
+                        empleado.getSueldo());
+            }
+        }
+    }
+
+    void empleadosSalario(){
+
+        System.out.println("Introduce el sueldo del empleado que quieres buscar");
+            String sueldo = teclat.nextLine();
+
+        System.out.println("ID       | Nombre    | Apellidos     | Sueldo\n---------------------------------------");
+
+        for (int iterador = 0; iterador < raizBBDD.getEmpleados().getEmpleado().size(); iterador++)  {
+
+            EmpleadoType empleado = raizBBDD.getEmpleados().getEmpleado().get(iterador);
+
+            if (raizBBDD.getEmpleados().getEmpleado().get(iterador).getSueldo().equalsIgnoreCase(sueldo)) {
+                System.out.println(empleado.getId() + " - " +
+                        empleado.getNombre() + "  -  " +
+                        empleado.getApellido() + "  ->  " +
+                        empleado.getSueldo());
+            }
+        }
+    }
+
+    void empleadosAntiguedad(){
+
+        System.out.println("Introduce la antiguedad del empleado que quieres buscar");
+            String antiguedad = teclat.nextLine();
+
+        System.out.println("ID       | Nombre    | Apellidos     | Sueldo\n---------------------------------------");
+
+        for (int iterador = 0; iterador < raizBBDD.getEmpleados().getEmpleado().size(); iterador++)  {
+
+            EmpleadoType empleado = raizBBDD.getEmpleados().getEmpleado().get(iterador);
+
+            if (raizBBDD.getEmpleados().getEmpleado().get(iterador).getAntiguedad().equalsIgnoreCase(antiguedad)) {
+                System.out.println(empleado.getId() + " - " +
+                        empleado.getNombre() + "  -  " +
+                        empleado.getApellido() + "  ->  " +
+                        empleado.getSueldo());
+            }
+        }
+    }
+
+    void facturasClientes(){
+
+        System.out.println("Introduce el DNI del cliente");
+            String dni = teclat.nextLine();
+
+        System.out.println("ID Producto | Impuestos | Precio Total\n---------------------------------------");
+
+        for (int iterador = 0; iterador < raizBBDD.getFacturas().getFactura().size(); iterador++)  {
+
+            if (raizBBDD.getFacturas().getFactura().get(iterador).getDniCliente().equalsIgnoreCase(dni)) {
+
+                FacturaType factura = raizBBDD.getFacturas().getFactura().get(iterador);
+
+                System.out.println(factura.getIdProducto() + "         -   " +
+                factura.getIva() + "      -     " +
+                factura.getPrecioTotal());
+            }
+        }
+    }
 }
+
+    /*
+    // JAXB Controler
+    JaxbContext = JAXBContext.newInstance(TiendaType.class);
+    Unmarshaller ums = JaxbContext.createUnmarshaller();
+    TiendaType bbddTienda = (TiendaType) ums.unmarshal(new File("/home/46465442z/IdeaProjects/Example_XML_API/src/Dao_Dao/bbdd.xml"));
+    System.out.println(bbddTienda.getClientes().getCliente().get(0).getDni());*/
